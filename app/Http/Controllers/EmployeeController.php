8dd2b315229dc276b8889d5968682d02dea66b1d
Tetraxion <lutfid955@gmail.com>
2023-06-16 07:08:06 +0700

    first commit

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,227 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Employee;
+use App\Models\Position;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
+
+
+class EmployeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+
+        $pageTitle = 'Employee List';
+
+        // ELOQUENT
+        $employees = Employee::all();
+
+        return view('employee.index', [
+            'pageTitle' => $pageTitle,
+            'employees' => $employees
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+
+    public function create()
+    {
+        // Lutfi
+        // $pageTitle = 'Create Employee';
+        // // RAW SQL Query
+        // $positions = DB::select('select * from positions');
+
+        // return view('employee.create', compact('pageTitle', 'positions'));
+
+        // $pageTitle = 'Create Employee';
+
+        // // Query Builder
+        // $positions = DB::table('positions')->get();
+
+        // return view('employee.create', compact('pageTitle', 'positions'));
+        $pageTitle = 'Create Employee';
+
+        // ELOQUENT
+        $positions = Position::all();
+
+        return view('employee.create', compact('pageTitle', 'positions'));
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // Mendefinisikan pesan kesalahan untuk validasi input
+        $messages = [
+            'required' => ':attribute harus diisi.',
+            'email' => 'Isi :attribute dengan format yang benar.',
+            'numeric' => 'Isi :attribute dengan angka.'
+        ];
+
+        // Validasi input menggunakan Validator
+        $validator = Validator::make($request->all(), [
+            'firstName' => 'required',
+            'lastName' => 'required',
+            'email' => 'required|email',
+            'age' => 'required|numeric',
+        ], $messages);
+
+        // Jika terdapat kesalahan validasi, kembalikan kembali ke halaman sebelumnya dengan pesan kesalahan dan input yang diisi sebelumnya
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+        // // INSERT QUERY
+        // DB::table('employees')->insert([
+        //     'firstname' => $request->firstName,
+        //     'lastname' => $request->lastName,
+        //     'email' => $request->email,
+        //     'age' => $request->age,
+        //     'position_id' => $request->position,
+        // ]);
+
+            // ELOQUENT
+        $employee = New Employee;
+        $employee->firstname = $request->firstName;
+        $employee->lastname = $request->lastName;
+        $employee->email = $request->email;
+        $employee->age = $request->age;
+        $employee->position_id = $request->position;
+        $employee->save();
+
+        return redirect()->route('employees.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        // Lutfi
+        // $pageTitle = 'Employee Detail';
+        // // RAW SQL QUERY
+        // $employee = collect(DB::select('
+        //     select *, employees.id as employee_id, positions.name as position_name
+        //     from employees
+        //     left join positions on employees.position_id = positions.id
+        //     where employees.id = ?
+        // ', [$id]))->first();
+
+        // return view('employee.show', compact('pageTitle', 'employee'));
+
+        $pageTitle = 'Employee Detail';
+
+        // // Query Builder
+        // $employee = DB::table('employees')
+        //     ->leftJoin('positions', 'employees.position_id', '=', 'positions.id')
+        //     ->select('employees.*', 'employees.id as employee_id', 'positions.name as position_name')
+        //     ->where('employees.id', '=', $id)
+        //     ->first();
+
+        // Lutfi
+        // ELOQUENT
+        $employee = Employee::find($id);
+
+        return view('employee.show', compact('pageTitle', 'employee'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+        $pageTitle = 'Edit Employee';
+
+        // // Query Builder
+        // $employee = DB::table('employees')
+        //     ->leftJoin('positions', 'employees.position_id', '=', 'positions.id')
+        //     ->select('employees.*', 'employees.id as employee_id', 'positions.name as position_name')
+        //     ->where('employees.id', '=', $id)
+        //     ->first();
+
+        // $positions = DB::table('positions')->get();
+          // ELOQUENT
+        $positions = Position::all();
+        $employee = Employee::find($id);
+
+
+        return view('employee.edit', compact('pageTitle', 'employee', 'positions'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $id)
+    {
+        // Mendefinisikan pesan kesalahan untuk validasi input
+        $messages = [
+            'required' => ':attribute harus diisi.',
+            'email' => 'Isi :attribute dengan format yang benar.',
+            'numeric' => 'Isi :attribute dengan angka.'
+        ];
+
+        // Validasi input menggunakan Validator
+        $validator = Validator::make($request->all(), [
+            'firstName' => 'required',
+            'lastName' => 'required',
+            'email' => 'required|email',
+            'age' => 'required|numeric',
+        ], $messages);
+
+        // Jika terdapat kesalahan validasi, kembalikan kembali ke halaman sebelumnya dengan pesan kesalahan dan input yang diisi sebelumnya
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+        // // UPDATE QUERY
+        // DB::table('employees')
+        //     ->where('id', $id)
+        //     ->update([
+        //         'firstname' => $request->firstName,
+        //         'lastname' => $request->lastName,
+        //         'email' => $request->email,
+        //         'age' => $request->age,
+        //         'position_id' => $request->position,
+        //     ]);
+
+          // ELOQUENT
+        $employee = Employee::find($id);
+        $employee->firstname = $request->firstName;
+        $employee->lastname = $request->lastName;
+        $employee->email = $request->email;
+        $employee->age = $request->age;
+        $employee->position_id = $request->position;
+        $employee->save();
+
+
+        return redirect()->route('employees.index');
+    }
+
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+        // // QUERY BUILDER
+        // DB::table('employees')
+        //     ->where('id', $id)
+        //     ->delete();
+          // ELOQUENT
+        Employee::find($id)->delete();
+
+        return redirect()->route('employees.index');
+    }
+}
